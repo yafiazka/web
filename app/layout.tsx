64f7aedb6128fd73dc1aedf6efc86a9f0593ebd1
Yafi Azka <yafiazka19@gmail.com>
2025-08-08 07:06:27 +0700

    feat/add-speed-insight-vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import LocalFont from "next/font/local";
 import { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: {
@@ -122,6 +123,7 @@ export default function RootLayout({
           }`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
